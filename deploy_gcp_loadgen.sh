359a3db12cd441def9271605e6b2891c39efa258
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-08-22 15:18:01 +0200

    added deployment option for new loadgen

---
+++ deploy_gcp_loadgen.sh
@@ -0,0 +1,17 @@
+export PROJECT_ID=`gcloud config get-value project`
+export ZONE=us-central1-a
+export CLUSTER_NAME=${PROJECT_ID}-1
+export MACHINE_TYPE=n1-standard-1		# smallest machine
+
+services=(adservice cartservice checkoutservice currencyservice emailservice frontend paymentservice prodcatservice recommservice shippingservice zipkin loadgenerator)
+
+gcloud container clusters create $CLUSTER_NAME --min-nodes=${#services[@]} --max-nodes=${#services[@]} --num-nodes=${#services[@]} --zone $ZONE --machine-type=${MACHINE_TYPE}
+
+nodes_string=`kubectl get nodes | grep -vP '^NAME' | grep -oP '^[\w\-0-9]+'`
+readarray -t nodes <<< "$nodes_string"
+
+for index in "${!services[@]}"; do kubectl label nodes ${nodes[index]} service=${services[index]}; done		# label each node for a specific service
+
+kubectl apply -f ./kubernetes-manifests
+
+kubectl get pods -o wide	# show deployment of pods for verification
