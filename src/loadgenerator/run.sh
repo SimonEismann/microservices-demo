7e60cc9636f815b09b0409433ead260cdbc46b87
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-08-24 13:24:49 +0200

    changed loadgen to 1 thread only to prevent homepage spam

--- src/loadgenerator/run.sh
+++ src/loadgenerator/run.sh
@@ -3,6 +3,6 @@
 # delay start to let system boot up
 sleep 120
 # start the actual loadgenerator and the loadgen director with a specified .lua load script.
-java -jar httploadgenerator.jar loadgenerator & java -cp load.jar:httploadgenerator.jar tools.descartes.dlim.httploadgenerator.runner.Main director --ip localhost --load load.csv -o result.csv --lua load.lua -t 256 -c measurment.ProcListener
+java -jar httploadgenerator.jar loadgenerator & java -cp load.jar:httploadgenerator.jar tools.descartes.dlim.httploadgenerator.runner.Main director --ip localhost --load load.csv -o result.csv --lua load.lua -t 1 -c measurment.ProcListener
 cat result.csv
 while true; do sleep 10; done	# keep the container from rebooting
