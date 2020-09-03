672421b4211b55340c6b9abb4cbda9413bfd3a4b
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-09-03 12:16:49 +0200

    populator now logs the item amount. Item amount at least 1 now.

--- util/cart-populator/populator.go
+++ util/cart-populator/populator.go
@@ -26,10 +26,10 @@ func cartItemsToString(items *[]*pb.CartItem) *string {
 
 func connectToRedis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     REDIS_ADDR,
-		Password: "", // no password set
-		DB:       0,  // use default DB
-		MaxRetries: 5, // like in the c# implementation
+		Addr:       REDIS_ADDR,
+		Password:   "", // no password set
+		DB:         0,  // use default DB
+		MaxRetries: 5,  // like in the c# implementation
 	})
 	/*_, err := rdb.Ping(c).Result()
 	if err != nil {
@@ -39,20 +39,20 @@ func connectToRedis() *redis.Client {
 }
 
 var (
-	PRODUCTS = [...]string{"0PUK6V6EV0", "1YMWWN1N4O", "2ZYFJ3GM2N", "66VCHSJNUP", "6E92ZMYYFZ", "9SIQT8TOJO", "L9ECAV7KIM", "LS4PSXUNUM", "OLJCESPC7Z"}
+	PRODUCTS   = [...]string{"0PUK6V6EV0", "1YMWWN1N4O", "2ZYFJ3GM2N", "66VCHSJNUP", "6E92ZMYYFZ", "9SIQT8TOJO", "L9ECAV7KIM", "LS4PSXUNUM", "OLJCESPC7Z"}
 	REDIS_ADDR string
-    wg sync.WaitGroup
+	wg         sync.WaitGroup
 )
 
 const (
-	START_INDEX int64 = 100000000
-	QUANTITY int32 = 5
-	CARTS_PER_THREAD = 100.0
+	START_INDEX      int64 = 100000000
+	QUANTITY         int32 = 5
+	CARTS_PER_THREAD       = 100.0
 )
 
 func main() {
-	args := os.Args[1:]		// first: redis address, second: amount of carts, third: item amount per cart
-	REDIS_ADDR = args [0]
+	args := os.Args[1:] // first: redis address, second: amount of carts, third: item amount per cart
+	REDIS_ADDR = args[0]
 	cartAmount, err := strconv.ParseInt(args[1], 10, 64)
 	if err != nil {
 		fmt.Println(err)
@@ -66,10 +66,10 @@ func main() {
 	for i := 0; i < threadAmount; i++ {
 		offset := int64(CARTS_PER_THREAD * i)
 		carts := int64(CARTS_PER_THREAD)
-		if i >= threadAmount - 1 {
+		if i >= threadAmount-1 {
 			carts = cartAmount - offset
 		}
-		go addCart(START_INDEX + offset, itemAmount, carts)	// adds cart in new thread
+		go addCart(START_INDEX+offset, itemAmount, carts) // adds cart in new thread
 	}
 	wg.Wait()
 }
@@ -77,11 +77,12 @@ func main() {
 func addCart(cart_index_start int64, itemAmount int64, cartAmount int64) {
 	defer wg.Done()
 	client := connectToRedis()
-	for i := cart_index_start; i < cart_index_start + cartAmount; i++ {
+	for i := cart_index_start; i < cart_index_start+cartAmount; i++ {
 		items := []*pb.CartItem{}
-		for j := 0; j < int(rand.NormFloat64() * float64(itemAmount) / 3 + float64(itemAmount)); j++ {		// normal distribution of itemAmount with with stddev=itemAmount/3
+		finalItemAmount := int(math.Max(rand.NormFloat64()*float64(itemAmount)/3+float64(itemAmount), 1.0)) // normal distribution of itemAmount with with stddev=itemAmount/3
+		for j := 0; j < finalItemAmount; j++ {
 			items = append(items, &pb.CartItem{
-				ProductId: PRODUCTS[j % len(PRODUCTS)],
+				ProductId: PRODUCTS[j%len(PRODUCTS)],
 				Quantity:  QUANTITY,
 			})
 		}
@@ -89,7 +90,7 @@ func addCart(cart_index_start int64, itemAmount int64, cartAmount int64) {
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Printf("cart %d added\n", i)
+			fmt.Printf("cart %d added with %d items\n", i, finalItemAmount)
 		}
 	}
 	err := client.Close()
