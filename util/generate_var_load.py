38d2b2b861565f8721682ff7da1f1e7f8179c1e8
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-09-30 14:18:09 +0200

    bugfixes and improvements

--- util/generate_var_load.py
+++ util/generate_var_load.py
@@ -7,17 +7,18 @@ max = int(sys.argv[3])
 length = int(sys.argv[4])
 step = 1
 
-currentLoad = min
+currentLoad = 1
 direction = True    #true = load rising, false = load decreasing
 
 writer = csv.writer(open(output_file, "w", newline=''), delimiter=',')
 for second in range(length):
-    if(direction):
-        currentLoad += step
-        if(currentLoad >= max):
-            direction = False
-    else:
-        currentLoad -= step
-        if(currentLoad <= min):
-            direction = True
+    if(second % 2 == 0):
+        if(direction):
+            currentLoad += step
+            if(currentLoad >= max):
+                direction = False
+        else:
+            currentLoad -= step
+            if(currentLoad <= min):
+                direction = True
     writer.writerow([second + 0.5, currentLoad])
--- util/parse_training_data.py
+++ util/parse_training_data.py
@@ -7,7 +7,7 @@ OUTPUT_FILE_NAME = sys.argv[2]
 FILES = sys.argv[3].split(",")     # all files to include for this operation separated by comma
 
 dataframe = pd.concat([pd.read_csv(TRAINING_DATA_FOLDER_PATH + "/" + f, sep=',', header=None, names=["start", "end"]) for f in FILES], ignore_index=True)
-dataframe.sort_values('start')
+dataframe = dataframe.sort_values(by=['start'])
 open_requests = []
 writer = csv.writer(open(TRAINING_DATA_FOLDER_PATH + "/" + OUTPUT_FILE_NAME, "w", newline=''), delimiter=',')
 writer.writerow(["Response Time", "Concurrency"])
