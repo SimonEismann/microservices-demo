322315e937e2cb8b8311979c10ec04bbd3ab47bd
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2020-11-20 15:36:31 +0100

    added rta calc to script

--- util/overview_to_table.py
+++ util/overview_to_table.py
@@ -6,6 +6,22 @@ LOADS = [5, 10, 15, 20, 25, 30]
 
 PATTERN = re.compile("^(\w+):.+avg\. response time: ([\d\.]+),.*avg\. utilization: ([\d\.]+),.*")
 
+def getClientPattern(service_name):
+    return re.compile("^" + service_name + " CLIENT:.+avg\. response time: ([\d\.]+),.*")
+
+def getClientAvgResponseTime(service_name, load):
+    rts = []
+    ptrn = getClientPattern(service_name)
+    for run in RUNS:
+        file = open("checkout-" + str(run) + "-" + str(load) + "/overview.txt")
+        for line in file:
+            res = ptrn.match(line)
+            if res:
+                rts.append(float(res.group(1)))
+                break
+        file.close()
+    return sum(rts) / len(rts)
+
 # config for specific setup
 ITEM_AMOUNT_PER_CART = 1    # how many items are in cart? (constant)
 RECOMMENDATION_AMOUNT = 1   # how many recommendations does frontend show on page? (constant)
@@ -74,11 +90,33 @@ for column in df_sdl:
     means.append(df_sdl[column].mean() * CORE_COUNT * 10)
 df_sdl = df_sdl.append(pandas.DataFrame(data=[means], index=["SDL of MEAN"], columns=df_sdl.columns))
 
+# RTA calculation
+df_rta = df_rt.copy(deep=True)
+
+def getRTA(service_name, avg_rt, load):
+    rta = avg_rt
+    if service_name == "recommendationservice":
+        rta -= getClientAvgResponseTime("productcatalogservice", load)
+    elif service_name == "checkoutservice":
+        rta -= 2*getClientAvgResponseTime("cartservice", load) + 2*getClientAvgResponseTime("shippingservice", load) + (ITEM_AMOUNT_PER_CART + 1)*getClientAvgResponseTime("currencyservice", load) + ITEM_AMOUNT_PER_CART*getClientAvgResponseTime("productcatalogservice", load) + getClientAvgResponseTime("emailservice", load) + getClientAvgResponseTime("paymentservice", load)
+    elif service_name == "frontend":
+        rta -= getClientAvgResponseTime("recommendationservice", load) + getClientAvgResponseTime("checkoutservice", load) + RECOMMENDATION_AMOUNT*getClientAvgResponseTime("productcatalogservice", load)
+    return rta
+
+for column in df_rta:
+    for i in range(len(LOADS)):
+        rt_avg = df_rta.at[LOADS[i], column]
+        rta = getRTA(column, rt_avg, LOADS[i])
+        df_rta.at[LOADS[i], column] = rta
+
+# final result save
 with pandas.ExcelWriter('overview_table.xlsx') as writer:
     df_util.to_excel(writer, sheet_name='Utilizations')
     df_sdl.to_excel(writer, sheet_name='SDL Values')
     df_rt.to_excel(writer, sheet_name='Response Times')
+    df_rta.to_excel(writer, sheet_name='RTA Values')
 
 print(df_util)
 print(df_sdl)
 print(df_rt)
+print(df_rta)
